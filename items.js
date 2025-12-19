ad5ee3604bfc16ec53612ca4a99385c344227da8
Rannyyyyyyy <23-18644@g.batstate-u.edu.ph>
2025-12-19 23:49:26 +0800

    Mobile Legends Hero and Build Part 2
    Items

---
+++ items.js
@@ -0,0 +1,73 @@
+import antiqueCuirassImg from "../assets/items/antique-cuirass.jpg";
+import athenasShieldImg from "../assets/items/athenas-shield.png";
+import berserkersFuryImg from "../assets/items/berserkers-fury.webp";
+import bladeOfDespairImg from "../assets/items/blade-of-despair.png";
+import bladeOfHeptaseasImg from "../assets/items/blade-of-heptaseas.png";
+import bloodWingsImg from "../assets/items/blood-wings.png";
+import bloodlustAxeImg from "../assets/items/bloodlust-axe.jpg";
+import bruteForceBreastplateImg from "../assets/items/brute-force-breastplate.png";
+import demonHunterSwordImg from "../assets/items/demon-hunter-sword.png";
+import demonShoesImg from "../assets/items/demon-shoes.png";
+import divineGlaiveImg from "../assets/items/divine-glaive.png";
+import dominanceIceImg from "../assets/items/dominance-ice.webp";
+import enchantedTalismanImg from "../assets/items/enchanted-talisman.png";
+import endlessBattleImg from "../assets/items/endless-battle.png";
+import fleetingTimeImg from "../assets/items/fleeting-time.png";
+import geniusWandImg from "../assets/items/genius-wand.png";
+import glowingWandImg from "../assets/items/glowing-wand.png";
+import goldenStaffImg from "../assets/items/golden-staff.webp";
+import haasClawImg from "../assets/items/haas-claw.png";
+import holyCrystalImg from "../assets/items/holy-crystal.webp";
+import hunterStrikeImg from "../assets/items/hunter-strike.png";
+import iceQueenWandImg from "../assets/items/ice-queen-wand.png";
+import immortalityImg from "../assets/items/immortality.png";
+import lightningTruncheonImg from "../assets/items/lightning-truncheon.png";
+import magicShoesImg from "../assets/items/magic-shoes.png";
+import maleficGunImg from "../assets/items/malefic-gun.png";
+import maleficRoarImg from "../assets/items/malefic-roar.png";
+import oracleImg from "../assets/items/oracle.png";
+import queensWingsImg from "../assets/items/queens-wings.png";
+import radiantArmorImg from "../assets/items/radiant-armor.png";
+import seaHalberdImg from "../assets/items/sea-halberd.png";
+import swiftBootsImg from "../assets/items/swift-boots.webp";
+import toughBootsImg from "../assets/items/tough-boots.webp";
+import warriorBootsImg from "../assets/items/warrior-boots.jpg";
+import windtalkerImg from "../assets/items/windtalker.png";
+
+export const items = [
+  { name: "Antique Cuirass", img: antiqueCuirassImg, type: "defense" },
+  { name: "Athena's Shield", img: athenasShieldImg, type: "defense" },
+  { name: "Berserker's Fury", img: berserkersFuryImg, type: "physical" },
+  { name: "Blade of Despair", img: bladeOfDespairImg, type: "physical" },
+  { name: "Blade of the Heptaseas", img: bladeOfHeptaseasImg, type: "physical" },
+  { name: "Blood Wings", img: bloodWingsImg, type: "magic" },
+  { name: "Bloodlust Axe", img: bloodlustAxeImg, type: "physical" },
+  { name: "Brute Force Breastplate", img: bruteForceBreastplateImg, type: "defense" },
+  { name: "Demon Hunter Sword", img: demonHunterSwordImg, type: "physical" },
+  { name: "Demon Shoes", img: demonShoesImg, type: "boots" },
+  { name: "Divine Glaive", img: divineGlaiveImg, type: "magic" },
+  { name: "Dominance Ice", img: dominanceIceImg, type: "defense" },
+  { name: "Enchanted Talisman", img: enchantedTalismanImg, type: "magic" },
+  { name: "Endless Battle", img: endlessBattleImg, type: "physical" },
+  { name: "Fleeting Time", img: fleetingTimeImg, type: "magic" },
+  { name: "Genius Wand", img: geniusWandImg, type: "magic" },
+  { name: "Glowing Wand", img: glowingWandImg, type: "magic" },
+  { name: "Golden Staff", img: goldenStaffImg, type: "physical" },
+  { name: "Haas's Claws", img: haasClawImg, type: "physical" },
+  { name: "Holy Crystal", img: holyCrystalImg, type: "magic" },
+  { name: "Hunter Strike", img: hunterStrikeImg, type: "physical" },
+  { name: "Ice Queen Wand", img: iceQueenWandImg, type: "magic" },
+  { name: "Immortality", img: immortalityImg, type: "defense" },
+  { name: "Lightning Truncheon", img: lightningTruncheonImg, type: "magic" },
+  { name: "Magic Shoes", img: magicShoesImg, type: "boots" },
+  { name: "Malefic Gun", img: maleficGunImg, type: "physical" },
+  { name: "Malefic Roar", img: maleficRoarImg, type: "physical" },
+  { name: "Oracle", img: oracleImg, type: "defense" },
+  { name: "Queen's Wings", img: queensWingsImg, type: "defense" },
+  { name: "Radiant Armor", img: radiantArmorImg, type: "defense" },
+  { name: "Sea Halberd", img: seaHalberdImg, type: "physical" },
+  { name: "Swift Boots", img: swiftBootsImg, type: "boots" },
+  { name: "Tough Boots", img: toughBootsImg, type: "boots" },
+  { name: "Warrior Boots", img: warriorBootsImg, type: "boots" },
+  { name: "Windtalker", img: windtalkerImg, type: "physical" },
+];
